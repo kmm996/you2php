3a0442267bc7221851e1343497dca7017c209ecf
Jaz wu <592015984b@gmail.com>
2020-01-24 17:49:17 +0800

    Add files via upload

---
+++ us/config.php
@@ -0,0 +1,10 @@
+<?php
+define('ROOT_PART', Root_part());
+#US- us222
+define('APIKEY', 'xxxxxx-xxxxxx');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'Youtube - Doge');
+define('TITLENAME', 'Youtube - Doge');
+define('EN2DEKEY', 'xxxxxx');
+define('EMAIL', '996@live.in');
+?>
