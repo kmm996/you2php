5faf1fd6da67c1feab884e8db83c863648633921
Jaz wu <592015984b@gmail.com>
2020-01-24 17:49:20 +0800

    Add files via upload

---
+++ tw2/config.php
@@ -0,0 +1,10 @@
+<?php
+define('ROOT_PART', Root_part());
+#tw2- 4w2222
+define('APIKEY', 'xxxxxx');
+define('GJ_CODE', 'TW');
+define('SITE_NAME', 'Youtube - Doge');
+define('TITLENAME', 'Youtube - Doge');
+define('EN2DEKEY', 'xxxxxx');
+define('EMAIL', '996@live.in');
+?>
---
+++ tw3/config.php
@@ -0,0 +1,10 @@
+<?php
+define('ROOT_PART', Root_part());
+#tw3- tw333
+define('APIKEY', 'xxxxxx-xxxxxx');
+define('GJ_CODE', 'TW');
+define('SITE_NAME', 'Youtube - Doge');
+define('TITLENAME', 'Youtube - Doge');
+define('EN2DEKEY', 'xxxxxx');
+define('EMAIL', '996@live.in');
+?>
